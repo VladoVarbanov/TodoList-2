51491646a78b43e56d94f3bbe40eecdbdf879fc3
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2025-02-21 20:02:35 +0200

    Update Tabs.jsx

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Tabs() {
-  return <nav></nav>;
+  const tabs = ["All", "Open", "Completed"];
+  return (
+    <nav className="tab-container">
+      {tabs.map((tab, tabIndex) => {
+        return (
+          <button key={tabIndex} className="tab-button">
+            <h4>{tab}</h4>
+          </button>
+        );
+      })}
+    </nav>
+  );
 }
