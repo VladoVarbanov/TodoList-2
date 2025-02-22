8e8cb465ad6e1bd6822107858274708c6a472e3f
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2025-02-22 10:08:36 +0200

    Finishing Tabs, TodoCard and TodoList components

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-export default function Tabs() {
+export default function Tabs(props) {
+  const { todos } = props;
   const tabs = ["All", "Open", "Completed"];
   return (
     <nav className="tab-container">
       {tabs.map((tab, tabIndex) => {
+        const numOfTasks =
+          tab === "All"
+            ? todos.length
+            : tab === "Open"
+            ? todos.filter((val) => !val.complete).length
+            : todos.filter((val) => val.complete).length;
+
         return (
           <button key={tabIndex} className="tab-button">
-            <h4>{tab}</h4>
+            <h4>
+              {tab} <span>({numOfTasks})</span>
+            </h4>
           </button>
         );
       })}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import TodoCard from "./TodoCard.jsx";
 
-export default function TodoList() {
+export default function TodoList(props) {
+  const { todos } = props;
+  const tab = "All";
+  const filterTodosList =
+    tab === "All"
+      ? todos
+      : tab === "Complete"
+      ? todos.filter((val) => val.complete)
+      : todos.filter((val) => !val.complete);
   return (
-    <div>
-      <TodoCard />
-    </div>
+    <>
+      {filterTodosList.map((todo, todoIndex) => {
+        return <TodoCard key={todoIndex} todo={todo} />;
+      })}
+    </>
   );
 }
